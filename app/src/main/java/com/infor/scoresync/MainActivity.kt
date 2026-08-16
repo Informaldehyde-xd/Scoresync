270767f3cde979a657d51ec8c44ee3a7802398b3
Informaldehyde-xd <kubeshkhan565@gmail.com>
2026-08-17 00:35:37 +0530

    Implement gesture detection for long press action
    Added gesture detection to trigger sequence playback on long press.

--- app/src/main/java/com/infor/scoresync/MainActivity.kt
+++ app/src/main/java/com/infor/scoresync/MainActivity.kt
@@ -2,6 +2,10 @@ package com.infor.scoresync
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.GestureDetector
+import android.view.MotionEvent
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
@@ -9,8 +13,9 @@ import androidx.appcompat.app.AppCompatActivity
 class MainActivity : AppCompatActivity() {
 
     private lateinit var webView: WebView
+    private val handler = Handler(Looper.getMainLooper())
 
-    @SuppressLint("SetJavaScriptEnabled")
+    @SuppressLint("SetJavaScriptEnabled", "ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         Thread.setDefaultUncaughtExceptionHandler(CrashHandler(applicationContext))
         super.onCreate(savedInstanceState)
@@ -22,6 +27,27 @@ class MainActivity : AppCompatActivity() {
         webView.settings.domStorageEnabled = true
         webView.webViewClient = WebViewClient()
 
+        val gestureDetector = GestureDetector(this, object : GestureDetector.SimpleOnGestureListener() {
+            override fun onLongPress(e: MotionEvent) {
+                playSequence()
+            }
+        })
+        webView.setOnTouchListener { _, event ->
+            gestureDetector.onTouchEvent(event)
+            false
+        }
+
         webView.loadUrl("file:///android_asset/osmd/index.html")
+    }
+
+    private fun playSequence() {
+        val events = MidiParser.parse(this, "osmd/test.mid")
+        val noteOnEvents = events.filter { it.isNoteOn }.sortedBy { it.timeMs }
+
+        for (event in noteOnEvents) {
+            handler.postDelayed({
+                webView.evaluateJavascript("cursorNext();", null)
+            }, event.timeMs)
+        }
     }
 }
